52fd99306df4e6269c516092f8e3b418c50493d7
Regis Medeiros <medeirosregis90@gmail.com>
2024-04-28 21:02:55 -0300

    Mudanças na disposição do gráfico de processos

--- Frontend/src/app/process-chart/process-chart.component.ts
+++ Frontend/src/app/process-chart/process-chart.component.ts
@@ -8,62 +8,67 @@ declare var google: any;
   styleUrls: ['./process-chart.component.scss']
 })
 export class ProcessChartComponent implements OnInit {
-  data: any;
-  chart: any;
 
   constructor(private processService: ProcessService) { }
 
   ngOnInit(): void {
     google.charts.load('current', { 'packages': ['corechart'] });
-    google.charts.setOnLoadCallback(this.initializeChart.bind(this));
+    google.charts.setOnLoadCallback(this.processChart.bind(this));
+
+    setInterval(() => {
+      this.processChart();
+    }, 1000);
   }
 
-  initializeChart() {
-    this.data = new google.visualization.DataTable();
-    this.data.addColumn('string', 'Tempo');
-    this.data.addColumn('number', 'Uso da CPU (%)');
-    this.chart = new google.visualization.LineChart(document.getElementById('process_chart'));
-    this.updateChart();
-    setInterval(() => this.updateChart(), 2000); 
-  }
+  processChart() {
+    const currentTime = new Date();
+    const startTime = new Date(currentTime.getTime() - 30000); 
 
-  updateChart() {
-    const currentTime = new Date(); // Usar o tempo local do cliente
-    const time = `${this.padZero(currentTime.getHours())}:${this.padZero(currentTime.getMinutes())}:${this.padZero(currentTime.getSeconds())}`;
-    
     this.processService.getProcesses().subscribe(processes => {
-      this.data.removeRows(0, this.data.getNumberOfRows()); // Limpar dados existentes
-      processes.forEach(process => {
-        this.data.addRow([time, process.cpuUsage]); // Usar o mesmo tempo para todos os dados
+      const data = new google.visualization.DataTable();
+      data.addColumn('datetime', 'Tempo');
+      data.addColumn('number', 'Uso em (%)');
+
+      const rows = processes.map((process, index) => {
+        const time = new Date(currentTime.getTime() - (processes.length - index) * 2000);
+        return [time, process.cpuUsage];
       });
-      this.chart.draw(this.data, this.getChartOptions());
+
+      
+      const filteredRows = rows.filter(row => row[0] >= startTime && row[0] <= currentTime);
+
+      data.addRows(filteredRows);
+
+      const options = {
+        title: 'Uso da CPU (%)',
+        legend: { position: 'none' },
+        backgroundColor: '#f5f5f5',
+        colors: ['#007bff'],
+        lineWidth: 2, 
+
+        hAxis: {
+          title: 'Tempo',
+          format: 'HH:mm:ss',
+          titleTextStyle: { color: '#333' },
+          textStyle: { color: '#333' },
+          gridlines: { color: '#ddd' } 
+        },
+        vAxis: {
+          title: 'Uso em (%)',
+          titleTextStyle: { color: '#333' },
+          textStyle: { color: '#333' },
+          gridlines: { color: '#ddd' },
+          minValue: 0,
+          maxValue: 100,
+          ticks: [0, 20, 40, 60, 80, 100]  
+        }
+      };
+
+      const chart = new google.visualization.LineChart(document.getElementById('process_chart'));
+      chart.draw(data, options);
     });
   }
-  
-
-  getChartOptions() {
-    return {
-      title: 'Uso total da CPU',
-      backgroundColor: '#f5f5f5',
-      colors: ['#007bff'],
-      lineWidth: 2,
-      hAxis: {
-        title: 'Tempo',
-        titleTextStyle: { color: '#333' },
-        textStyle: { color: '#333' }
-      },
-      vAxis: {
-        title: '',
-        titleTextStyle: { color: '#333' },
-        textStyle: { color: '#333' },
-        minValue: 0,
-        maxValue: 100
-      }
-    };
-  }
-
-  padZero(num: number): string {
-    return num < 10 ? `0${num}` : num.toString();
-  }
 }
+
+
 
